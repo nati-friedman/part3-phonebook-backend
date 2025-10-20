06a34c62fb9a7cc8a31f107b33e6fa2897d861ad
Nati Friedman <nati.frdmn@gmail.com>
2025-10-20 17:21:50 +0300

    exercise 1.3

--- part1/courseinfo/courseinfo/src/Content.jsx
+++ part1/courseinfo/courseinfo/src/Content.jsx
@@ -3,9 +3,9 @@ import Part from "./Part";
 const Content = (props) => {
   return (
     <div>
-      <Part name={props.part1} exercises={props.exercises1} />
-      <Part name={props.part2} exercises={props.exercises2} />
-      <Part name={props.part3} exercises={props.exercises3} />
+      <Part name={props.name1} exercises={props.exercises1} />
+      <Part name={props.name2} exercises={props.exercises2} />
+      <Part name={props.name3} exercises={props.exercises3} />
     </div>
   );
 };
