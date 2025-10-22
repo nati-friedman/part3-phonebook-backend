d650fecca63ae0a8fe7f2ac75e64730db7c92661
Nati Friedman <nati.frdmn@gmail.com>
2025-10-22 08:27:05 +0300

    exercise 2.6

---
+++ part2/phonebook/phonebook/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // add new name to phonebook
+    setPersons(
+      persons.concat({
+        name: newName,
+      })
+    );
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
