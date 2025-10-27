f739c54de66a6bba430542672c1b41f2bc2f25de
Nati Friedman <nati.frdmn@gmail.com>
2025-10-27 22:11:00 +0200

    exercise 3.11

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const cors = require("cors");
 const morgan = require("morgan");
 
-morgan.token('data', req => {
-    return JSON.stringify(req.body)
-})
+morgan.token("data", (req) => {
+  return JSON.stringify(req.body);
+});
 
 const app = express();
 
-app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 
 let persons = [
   {
