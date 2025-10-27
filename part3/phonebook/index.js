0704a5c9e296bdd4f25402df852cfd32023ad465
Nati Friedman <nati.frdmn@gmail.com>
2025-10-27 02:58:11 +0200

    exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -24,16 +24,27 @@ let persons = [
   },
 ];
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
-
 app.get("/info", (request, response) => {
   const info = `
         Phonebook has info for ${persons.length} people<br>
         ${new Date()}
         `;
   response.send(info);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end()
+  }
 });
 
 const PORT = 3001;
