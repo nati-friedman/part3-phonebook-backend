35e8426c8c2c8faa8f7af1b581985d3a2ad57433
nati-friedman <112993844+nati-friedman@users.noreply.github.com>
2025-10-27 21:39:27 +0200

    Serve static files from 'dist' directory

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('data', req => {
 const app = express();
 
 app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"));
 
