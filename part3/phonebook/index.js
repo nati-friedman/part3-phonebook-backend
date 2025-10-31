0772ba95482a73346a17c1f229f424c23aa70b2e
Nati Friedman <nati.frdmn@gmail.com>
2025-10-31 23:50:03 +0200

    exercise 3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -15,35 +15,14 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/info", (request, response) => {
-  const info = `
-        Phonebook has info for ${persons.length} people<br>
+  Person.countDocuments({}).then((count) => {
+    const info = `
+        Phonebook has info for ${count} people<br>
         ${new Date()}
         `;
-  response.send(info);
+    response.send(info);
+  });
 });
 
 app.get("/api/persons", (request, response) => {
@@ -53,14 +32,15 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (!person) {
+        return response.status(404).end();
+      }
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+      return response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
